8f3b704f9502600d184f720a3be7ac885e293eb9
Vitaliy Ptitsyn <VitaliyPtitsyn@gmail.com>
2025-02-05 23:57:11 +0200

    Time: 4 ms (57.84%), Space: 39.6 MB (34.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val diffMap = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, int ->
+            diffMap[int]?.let { return intArrayOf(it, index) }
+            diffMap[target - int] = index
+        }
+        return intArrayOf()
+
+    }
+}
