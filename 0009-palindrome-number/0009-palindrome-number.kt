288a1400c8bae7d12dd3f5d383f9afe50e88ed7b
Vitaliy Ptitsyn <VitaliyPtitsyn@gmail.com>
2025-02-10 23:55:51 +0200

    Time: 5 ms (100%), Space: 38.1 MB (40.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,5 +1,16 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        return   x.toString().reversed() ==  x.toString()
+         var normalNumber = x
+        var reverseNumber = 0
+        
+        if(x < 0) return false
+
+        while(normalNumber > 0) {
+            var rest = normalNumber % 10 
+            reverseNumber = (reverseNumber * 10) + rest
+            normalNumber = normalNumber / 10
+        }
+
+        return reverseNumber == x
     }
 }
