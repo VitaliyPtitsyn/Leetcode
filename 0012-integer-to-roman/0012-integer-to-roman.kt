e82648523d412f2d6be75305f32189d4fffe4447
Vitaliy Ptitsyn <VitaliyPtitsyn@gmail.com>
2025-02-15 22:53:24 +0200

    Time: 12 ms (52.97%), Space: 38.8 MB (59.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.kt
+++ 0012-integer-to-roman/0012-integer-to-roman.kt
@@ -1,42 +1,34 @@
 class Solution {
     val romanNumberMap= hashMapOf<Int,String>(
-        Pair(1,"I",)    ,
-        Pair(5,"V")	,
-        Pair(10,"X")	,
-        Pair(50,"L")	,
-        Pair(100,"C")	,
-        Pair(500,"D")	,
-        Pair(1000,"M")	,
+        Pair(1,"I",),
+        Pair(5,"V"),
+        Pair(10,"X"),
+        Pair(50,"L"),
+        Pair(100,"C"),
+        Pair(500,"D"),
+        Pair(1000,"M"),
     )
     fun intToRoman(num: Int): String {
         val builder= StringBuilder()
         var reminder=num
         var numberSplitter=1000
-        System.out.println("num:  number $num  ")
-        while (reminder>0 && numberSplitter>0){
+        while (reminder>0){
            val number=reminder / numberSplitter
-            builder.append(number.toRoman(numberSplitter*10))
+            number.toRoman(builder,numberSplitter*10)
             reminder %= numberSplitter
             numberSplitter /= 10
         }
      return builder.toString()
     }
-    fun Int.toRoman(order:Int):String{
-        if(this==0) return ""
-        val builder= StringBuilder()
+    fun Int.toRoman(builder:StringBuilder,order:Int){
+        if(this==0) return
         val reminder= this%5
-        var endSymbol :String
-        var startSymbol :String =romanNumberMap[order/10] ?:""
-        if(this>5){
-            endSymbol= romanNumberMap[order] ?: ""
-        } else {
-            endSymbol= romanNumberMap[order/2] ?: ""
-        }
+        val startSymbol :String =romanNumberMap[order/10] ?:""
+        val endSymbol :String =  romanNumberMap[if(this>5) order else  order/2] ?: ""
         when(reminder){
             in 1..3 -> {
                 if(this> 5)  builder.append(romanNumberMap[order/2])
-                for(i in 1.. reminder)
-                   builder.append(startSymbol)
+                for(i in 1.. reminder) builder.append(startSymbol)
             }
             4 -> {
                 builder.append(startSymbol)
@@ -44,6 +36,5 @@ class Solution {
             }
             0  -> {builder.append(endSymbol)}
         }
-        return builder.toString()
     }
 }
